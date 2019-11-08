3462ef82761c84832e9e55935650f0d7fb26452e
Gruselhaus <nico@gruselhaus.com>
2019-11-08 23:19:22 +0100

    Edited colors

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let mappa;
 let planeMap;
 let canvas;
-let url = 'https://opensky-network.org/api/states/all?lamin=49.707526&lomin=7.997795&lamax=50.696854&lomax=9.673431';
+let url = "https://opensky-network.org/api/states/all?lamin=49.707526&lomin=7.997795&lamax=50.696854&lomax=9.673431";
 let time;
 const pos = {
   lat: 0,
   lon: 0,
   track: 0
-}
+};
 
 let visible = false;
 let params = null;
@@ -16,12 +16,12 @@ const options = {
   lng: 8.557677,
   zoom: 14,
   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-}
+};
 let planes = [];
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
-  mappa = new Mappa('Leaflet');
+  mappa = new Mappa("Leaflet");
   planeMap = mappa.tileMap(options);
   planeMap.overlay(canvas);
   getData();
@@ -36,7 +36,7 @@ const getData = async () => {
   for (const state of formatted.states) {
     planes.push(new Plane(state));
   }
-}
+};
 
 function draw() {
   clear();
@@ -58,12 +58,14 @@ class Plane {
     const pix = planeMap.latLngToPixel(lat, lon);
     const callsign = this.states[1];
     stroke(0);
-    strokeWeight(.5);
-    fill(200, 100, 123, 150);
-    ellipse(pix.x, pix.y, 12, 12);
+    strokeWeight(0.5);
     if (callsign === "") {
+      fill(240, 200, 123, 150);
+      ellipse(pix.x, pix.y, 12, 12);
       return;
     }
+    fill(100, 200, 123, 200);
+    ellipse(pix.x, pix.y, 12, 12);
     beginShape();
     stroke(0);
     strokeWeight(1);
@@ -72,7 +74,7 @@ class Plane {
     vertex(pix.x + 20, pix.y - 15);
     vertex(pix.x + 80, pix.y - 15);
     endShape();
-    strokeWeight(.2);
+    strokeWeight(0.2);
     fill(0);
     text(callsign, pix.x + 24, pix.y - 17);
   }
